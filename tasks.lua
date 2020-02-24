544e17fedcf770d896b814ec8ef111bc0c2f472a
Thiago Naves <naves.thiago@gmail.com>
2020-02-24 16:49:42 -0300

    Started new implementation

--- tasks.lua
+++ tasks.lua
@@ -1,380 +1,93 @@
-require "debugger"
---require("mobdebug").start()
---require("mobdebug").coro()
+local dbg = require "debugger"
+-- Observer
+event_t = {}
 
--- Coroutine wrapper
-co = {tasks={}}
-setmetatable(co.tasks, {__mode="kv"})
-
-function co.create(f)
-    local fwrap = function(...)
-        f(...)
-        local handle = co.running()
-        handle.state = "dead"
-        if handle.destructor then
-            handle.destructor(handle)
-        end
-    end
-
-    local ret = {handle = coroutine.create(fwrap), state = "suspended", destructor = nil}
-    co.tasks[ret.handle] = ret
-    ret.setdestructor = function(handle, f) handle.destructor = f end
-
-    return ret
+function event_t:new()
+	return setmetatable({listeners = {}, waiting = {}}, {__index = self, __call = self.__call})
 end
 
-function co.yield(...)
-    local handle = co.running()
-    if handle == nil then
-        return false, "Not running"
-    end
-
-    handle.state = "suspended"
-
-    return coroutine.yield(...)
+function event_t:listen(f)
+	self.listeners[f] = true
 end
 
-function co.resume(handle, ...)
-    if handle.state == "dead" then
-        return false, "Cannot resume a dead coroutine"
-    end
-
-    handle.state = "running"
-
-    local current = co.running()
-    if current then
-        current.state = "suspended"
-
---[[
-        if children[current] == nil then
-            children[current] = {}
-        end
-
-        if handle.parent == nil then
-            handle.parent = current
-
-            -- TODO change this to children[current] = true
-            table.insert(children[current], handle)
-        end
---]]
-    end
-
-    return coroutine.resume(handle.handle, ...)
+function event_t:await(f)
+	self.waiting[f] = true
 end
 
-function co.kill(handle)
-    if handle.state == "dead" then
-        -- Already dead...do nothing
-        return
-    end
-
-    if handle.state == "running" then
-        -- Kill self: yield back to the parent
-        handle.state = "dead"
-        if handle.destructor then
-            handle.destructor(handle)
-        end
-        coroutine.yield()
-        return
-    end
-
-    -- handle.state == "suspended"
-    handle.state = "dead"
-    if handle.destructor then
-        handle.destructor(handle)
-    end
+function event_t:remove_listener(f)
+	self.listeners[f] = nil
 end
 
-function co.running()
-    return co.tasks[coroutine.running()]
+function event_t:remove_await(f)
+	self.waiting[f] = nil
 end
 
-function co.wrap(f)
-    local c = co.create(f)
-    return function(...) co.resume(c, ...) end
+function event_t:__call(...)
+	for l in pairs(self.listeners) do
+		l(self, ...)
+	end
+	for l in pairs(self.waiting) do
+		l(self, ...)
+	end
+	self.waiting = {}
 end
 
-function co.state(handle)
-    return handle.state
+-- Task
+task_t = {}
+
+function task_t:new(f, name)
+	local t = {f = f, done = event_t:new(), state = "ready", parent = nil, coroutine = nil, name = name or "??"}
+	return setmetatable(t, {__index = self, __call = self.__call})
 end
 
-----------------------------------------
-
-children = {}
-setmetatable(children, {__mode="k"})
-
-function node_done_or(handle)
-    --pause()
-    local c = children[handle]
-    if c then
-        for i,j in pairs(c) do
-            j.parent = nil
-            if j.node_done then
-                j.node_done(j)
-            end
-            co.kill(j)
-        end
-    end
-
-    if handle.parent and handle.parent.node_done then
-        handle.parent.node_done(handle.parent)
-    end
-
-    handle.destructor = nil
-    co.kill(handle)
-    -- TODO remove from children lists
+function task_t:__call()
+	if self.state ~= "ready" then
+		return
+	end
+	self.state = "alive"
+	self.parent = scheduler.current
+	self.coroutine = coroutine.create(function() self.f() self:kill() end)
+	scheduler.current = self
+	if self.parent then
+		--self.parent.done:listen(function() print(self.name .. ": killed by parent ("..self.parent.name..")") self:kill() end)
+		self.parent.done:listen(function() self:kill() end)
+	end
+	coroutine.resume(self.coroutine)
 end
 
-function par_or(fa, fb, name)
-    local sub -- children list
-
-    local coa, cob
-    coa = co.create(fa)
-    coa.destructor = node_done_or
-
-    cob = co.create(fb)
-    cob.destructor = node_done_or
-
-    sub = {coa, cob}
-
-    return function()
-        local handle = co.running()
-        handle.node_done = node_done_or
-        handle.name = name -- debug
-
-        children[handle] = {}
-        for i,j in ipairs(sub) do
-            j.parent = handle
-            table.insert(children[handle], j)
-        end
-
-        for i,j in ipairs(sub) do
-            -- TODO figure out how to detect if a child is dead by now
-            co.resume(j)
-            if j.state == "dead" then
-                -- j finished without yielding.
-                -- stop starting tasks
-                --pause()
-                return
-            end
-        end
-
-        -- Only yield if coa and cob are still alive
---        for i,j in ipairs(sub) do
---            if j.state == "dead" then
---                return
---            end
---        end
-        co.yield()
-    end
+function task_t:kill()
+	--print('Kill ' .. self.name .. ' (' .. self.state .. ')')
+	if self.state == "dead" then
+		return
+	end
+	self.state = "dead"
+	scheduler.current = self.parent
+	self.done()
+	self.f = nil
+	--print(self.name .. ' killed')
+	self.done = nil
+	self.parent = nil
+	self.coroutine = nil
 end
 
-function node_done_and(handle)
-    local c = children[handle]
+scheduler = {current = nil, waiting = {}}
+function await(evt)
+	
+	local waiting = scheduler.waiting
+	if not scheduler.current then
+		print("no task")
+		return
+	end
 
-    if c then
-        for i,j in pairs(c) do
-            if j.state ~= "dead" then
-                return -- There is at least one child alive. Do nothing
-            end
-        end
-
-        for i,j in pairs(c) do
-            j.parent = nil
-            if j.node_done then
-                j.node_done(j)
-            end
-            co.kill(j)
-        end
-    end
-
-    if handle.parent and handle.parent.node_done then
-        handle.parent.node_done(handle.parent)
-    end
-
-    handle.destructor = nil
-    co.kill(handle)
-    -- TODO remove from children lists
+	if not waiting[evt] then
+		waiting[evt] = event_t:new()
+	end
+	waiting[evt]:await(function() coroutine.resume(scheduler.current.coroutine) end)
+	coroutine.yield()
 end
 
-function par_and(fa, fb, name)
-    local sub -- children list
-
-    local coa, cob
-    coa = co.create(fa)
-    coa.destructor = node_done_and -- these have only 1 child (or none). simply kill (same as the OR)
-
-    cob = co.create(fb)
-    cob.destructor = node_done_and -- these have only 1 child (or none). simply kill (same as the OR)
-
-    sub = {coa, cob}
-
-    return function()
-        local handle = co.running()
-        handle.node_done = node_done_and
-        handle.name = name -- debug
-
-        children[handle] = {}
-        for i,j in ipairs(sub) do
-            j.parent = handle
-            table.insert(children[handle], j)
-        end
-
-        for i,j in ipairs(sub) do
-            co.resume(j)
-        end
-
-        -- Only yield if a child is still alive
-        for i,j in ipairs(sub) do
-            if j.state == "suspended" then
-                co.yield()
-                break
-            end
-        end
-    end
-end
-
-----------------------------------------
-
-function pa()
-    ha = co.running()
-    print("ini a")
-    co.yield()
-    print("fim a")
-end
-
-function pb()
-    hb = co.running()
-    print("ini b")
-    co.yield()
-    print("fim b")
-end
-
-function pc()
-    hc = co.running()
-    print("ini c")
-    co.yield()
-    print("fim c")
-end
-
-function pd()
-    hd = co.running()
-    print("ini d")
-    co.yield()
-    print("fim d")
-end
-
-function start(f)
-    local current = co.running()
-    local sub = co.create(function()
-        local r = co.running()
-        local sub_sub = co.create(f)
-        r.sub = sub_sub
-        sub_sub.parent = r
-        children[r] = children[r] or {}
-        table.insert(children[r], sub_sub)
-        co.resume(sub_sub)
-        if sub_sub.state ~= "dead" then
-            co.yield()
-        end
-    end)
-    sub.node_done = function(handle)
-        handle.sub.parent = nil
-        handle.sub.node_done(handle.sub)
-        co.kill(handle.sub)
-        if current then
-            -- If the program ended, there will be no 'current' to resume
-            co.resume(current)
-        end
-    end
-    if current then
-        sub.parent = current
-        children[current] = children[current] or {}
-        table.insert(children[current], sub)
-    end
-    co.resume(sub)
-    if sub.state ~= "dead" then
-        co.yield()
-    end
-end
-
-function pe()
-    he = co.running()
-    print("ini e")
-
-    start(par_or(pf, pg))
---    print("antes yield")
---    co.yield()
-    print("fim e")
-end
-
-function pf()
-    hf = co.running()
-    print("ini f")
-    co.yield()
-    print("fim f")
-end
-
-function pg()
-    hg = co.running()
-    print("ini g")
-    co.yield()
-    print("fim g")
-end
-
-function ph()
-    hh = co.running()
-    print("ini h")
-    print("fim h")
-end
-
-function pi()
-    hi = co.running()
-    print("ini i")
-    print("fim i")
-end
-
-function pj()
-    hj = co.running()
-    print("ini j")
-
-    start(par_or(pf, ph))
-    print("fim j")
-end
-
-function pk()
-    hk = co.running()
-    print("ini k")
-
-    start(par_and(pf, ph))
-    print("fim k")
-end
-
-
-function main()
---    par_or(pa, pb)()
---    par_or(par_or(pa, pb), pc)()
---    par_or(par_or(pa, pb, "A"), par_or(pc, pd, "B"), "fora")()
---    par_or(par_or(pa, pb), par_or(pc, pd))()
-    par_or(par_or(pa, pb), pe)()
---    par_and(pa, pb)()
---    par_and(pa, par_and(pb, pc))()
---    par_or(pa, par_and(pb, pc))()
---    par_and(pa, par_or(pb, pc))()
---    par_or(par_and(pa, par_or(pb, pc)), pd)()
---    par_or(par_and(pa, pb), pc)()
-end
-
---co.wrap(main)()
---co.resume(ha)
-
-start(par_or(par_or(pa, pb), pk))
-co.resume(ha)
-print("hf.state: "..hf.state, "<- should be 'dead'")
-print("hk.state: "..hk.state, "<- should be 'dead'")
---start(par_or(par_or(pa, pb), pj))
---start(par_or(par_or(pa, pb), pe))
---start(par_and(pa, par_or(par_or(pb, pc), ph)))
---start(par_or(par_and(pa, pb), ph))
---start(par_or(par_or(pb, pi), pa))
---start(par_or(pa, par_and(ph, pb)))
+function ta() print("ta ini") await(1) print("ta fim") end
+t2 = task_t:new(ta, "A")
+function tb() print("tb ini") t2() print("tb fim") end
+t = task_t:new(tb, "B")
+t()
